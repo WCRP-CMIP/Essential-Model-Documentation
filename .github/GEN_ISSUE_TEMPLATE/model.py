5c840697f53121afbcb10cbb4902b535c9ef7952
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-15 23:40:12 +0000

    development

--- .github/GEN_ISSUE_TEMPLATE/model.py
+++ .github/GEN_ISSUE_TEMPLATE/model.py
@@ -1,9 +1,20 @@
 # Model Template Data
 from cmipld.utils.ldparse import graph_entry
 
+# Get scientific domains for embedded pairings
+domains = graph_entry('constants:scientific_domain/_graph.json')
+
+# Generate all pairings: a -> b where a != b
+embedded_pairs = []
+for a in domains:
+    for b in domains:
+        if a != b:
+            embedded_pairs.append(f"{a} -> {b}")
+
 DATA = {
-    'component': graph_entry('constants:model_component_type/_graph.json'),
+    'component': graph_entry('constants:scientific_domain/_graph.json'),
     'calendar': graph_entry('constants:calendar/_graph.json'),
     'model_family': graph_entry('emd:model_family/_graph.json'),
+    'embedded_pairs': sorted(embedded_pairs),
     'issue_kind': ['New', 'Modify']
 }
