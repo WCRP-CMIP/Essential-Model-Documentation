3d1b58a3be6b5302ebcf978509f662371cd7105f
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-15 23:41:52 +0000

    development

--- .github/GEN_ISSUE_TEMPLATE/model.py
+++ .github/GEN_ISSUE_TEMPLATE/model.py
@@ -10,11 +10,18 @@ for a in domains:
     for b in domains:
         if a != b:
             embedded_pairs.append(f"{a} -> {b}")
+            
+model_family = name_entry(
+    [i for i in cmipld.get('emd:model_family/_graph.json', 2).get('contents', []) 
+     if i.get('family_type') == 'model'],
+    value='ui_label',
+    key='validation_key'
+)
 
 DATA = {
     'component': graph_entry('constants:scientific_domain/_graph.json'),
     'calendar': graph_entry('constants:calendar/_graph.json'),
-    'model_family': graph_entry('emd:model_family/_graph.json'),
+    'model_family': model_family,
     'embedded_pairs': sorted(embedded_pairs),
     'issue_kind': ['New', 'Modify']
 }
