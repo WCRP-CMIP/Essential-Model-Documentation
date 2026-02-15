1ae6899f6f331c1264da27186021473aacb73dc1
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-15 22:22:25 +0000

    development

--- .github/GEN_ISSUE_TEMPLATE/model_component.py
+++ .github/GEN_ISSUE_TEMPLATE/model_component.py
@@ -1,8 +1,18 @@
 # Model Component Template Data
-from cmipld.utils.ldparse import graph_entry
+import cmipld
+from cmipld.utils.ldparse import graph_entry, name_entry
+
+component_family = name_entry(
+    [i for i in cmipld.get('emd:model_family/_graph.json', 2).get('contents', []) 
+     if i.get('family_type') == 'component'],
+    value='ui_label',
+    key='validation_key'
+)
 
 DATA = {
-    'component': graph_entry('constants:model_component_type/_graph.json'),
-    'component_family': graph_entry('emd:model_component/_graph.json'),
+    'component_type': graph_entry('constants:scientific_domain/_graph.json'),
+    'component_family': component_family,
+    'horizontal_grid': graph_entry('emd:horizontal_computational_grid/_graph.json'),
+    'vertical_grid': graph_entry('emd:vertical_computational_grid/_graph.json'),
     'issue_kind': ['New', 'Modify']
 }
