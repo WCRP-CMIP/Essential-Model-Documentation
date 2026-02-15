f374405fd2bbd7d17fc34a5386037fe19c7febfc
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-15 23:30:47 +0000

    development

--- .github/GEN_ISSUE_TEMPLATE/horizontal_computational_grid.py
+++ .github/GEN_ISSUE_TEMPLATE/horizontal_computational_grid.py
@@ -5,5 +5,6 @@ DATA = {
     'arrangement': graph_entry('constants:arrangement/_graph.json'),
     'grid_type': graph_entry('constants:grid_type/_graph.json'),
     'grid_mapping': graph_entry('constants:grid_mapping/_graph.json'),
+    'subgrids': graph_entry('emd:horizontal_subgrid/_graph.json'),
     'issue_kind': ['New', 'Modify']
 }
