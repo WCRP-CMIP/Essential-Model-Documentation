2e693585df54e795176245bde0b19d4608f1371f
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-25 22:35:22 +0000

    ;workflows and templates

--- .github/GEN_ISSUE_TEMPLATE/horizontal_computational_grid.py
+++ .github/GEN_ISSUE_TEMPLATE/horizontal_computational_grid.py
@@ -3,8 +3,8 @@ from cmipld.utils.ldparse import graph_entry
 
 DATA = {
     'arrangement': graph_entry('constants:arrangement/_graph.json'),
-    'grid_type': graph_entry('constants:grid_type/_graph.json'),
-    'grid_mapping': graph_entry('constants:grid_mapping/_graph.json'),
+    'cell_variable_type': graph_entry('constants:cell_variable_type/_graph.json'),
+    'horizontal_grid_cells': graph_entry('emd:horizontal_grid_cells/_graph.json'),
     'subgrids': graph_entry('emd:horizontal_subgrid/_graph.json'),
     'issue_kind': ['New', 'Modify']
 }
