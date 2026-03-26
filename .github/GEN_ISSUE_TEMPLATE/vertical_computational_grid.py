840f69562e17e770ed1c451e9033fc614879abb9
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-03-26 12:10:04 +0000

    development

--- .github/GEN_ISSUE_TEMPLATE/vertical_computational_grid.py
+++
@@ -1,7 +0,0 @@
-# Vertical Computational Grid Template Data
-from cmipld.utils.ldparse import graph_entry
-
-DATA = {
-    'vertical_coordinate': graph_entry('constants:vertical_coordinate/_graph.json'),
-    'issue_kind': ['New', 'Modify']
-}
